6774af94d250b9a5b297cf7764e0105f05495b41
Pedro Gracia <lasarux@MacBook-de-Pedro.local>
2016-09-12 21:02:55 +0200

    + add security access information

--- __openerp__.py
+++ __openerp__.py
@@ -24,7 +24,7 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'templates.xml',
         'views/telephony_isp.xml',
         'views/telephony_wizard.xml',
